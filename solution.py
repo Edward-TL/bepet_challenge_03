665089b7d7c6ca3672a15194ac57bca2730f7a5e
AndresBorda24 <ajborda4@misena.edu.co>
2021-06-26 16:01:22 -0500

    Solution third problem

--- solution.py
+++ solution.py
@@ -7,3 +7,11 @@ class Solution:
     def duplicate_zeros(self, arr):
         """Modifies arr in place duplicating all zeros while maitining the original length."""
         # YOUR SOLUTION GOES HERE
+        length = len(arr)
+
+        for i in range(1, length):
+            if arr[i] == 0 and arr[i -1] != 0:
+                arr.insert(i, 0)
+                arr.pop()
+
+        return arr
