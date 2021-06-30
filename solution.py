cbf5bd3e69dd0ac708c678ae7e14d4609472b098
victor06-vr <victormax000@gmail.com>
2021-06-29 23:08:10 -0500

    My solution challenge 3
    Empece usando un for para recorrer el arreglo, seguido puse una validacion para crear un salto de linea en caso que la posicion en la que vaya se cumpla, me haga un salto de linea

--- solution.py
+++ solution.py
@@ -7,3 +7,13 @@ class Solution:
     def duplicate_zeros(self, arr):
         """Modifies arr in place duplicating all zeros while maitining the original length."""
         # YOUR SOLUTION GOES HERE
+        x=len(arr)
+        vol=True
+        for cont in range(x):
+            if vol==False:
+                continue
+            if arr[cont]==0:
+                arr.insert(cont,0)
+                arr.pop()
+                vol=False
+            return arr
