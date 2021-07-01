a560f7b72889c391917d4ea041cfaece789af72e
LinaEspinosa <linaespinosa@gmail.com>
2021-06-30 23:29:02 -0500

    Avance reto 3

--- solution.py
+++ solution.py
@@ -4,6 +4,16 @@
 class Solution:
     """Solution to the duplicate zeros problem."""
 
-    def duplicate_zeros(self, arr):
-        """Modifies arr in place duplicating all zeros while maitining the original length."""
-        # YOUR SOLUTION GOES HERE
+def duplicate_zeros(self, arr):
+    """Modifies arr in place duplicating all zeros while maitining the original length."""
+    # YOUR SOLUTION GOES HERE
+    arr = [1,0,2,3,0,4,5,0]
+    i = 0
+    while i < len(arr):
+        if arr[i] == 0:
+            arr.insert(i+1,0)
+            del arr[len(arr)-1]
+            i = i + 2
+        else:
+            i = i + 1
+    return(self,arr)
